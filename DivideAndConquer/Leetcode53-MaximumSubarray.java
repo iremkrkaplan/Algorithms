cfb5478cf230a81c77d4496175ccfec5976200ab
İrem Karakaplan <115878341+iremkrkaplan@users.noreply.github.com>
2025-11-20 15:01:45 +0300

    Implement divide and conquer for maximum subarray problem
    Implemented the divide and conquer approach for finding the maximum subarray sum. Added helper methods for calculating the maximum subarray and crossing subarray sums.

--- DivideAndConquer/Leetcode53-MaximumSubarray.java
+++ DivideAndConquer/Leetcode53-MaximumSubarray.java
@@ -4,7 +4,40 @@ class Solution {
         return maxSubArrayHelper(nums, 0, nums.length - 1);
     }
 
-    private int maxSubArrayHelper(int[] nums, int left, int right) []
+    private int maxSubArrayHelper(int[] nums, int left, int right) {
+        if(left == right){
+            return nums[left];
+        }
 
-    private int maxCrossingSubArray
+        int mid = left + (right - left) / 2;
+
+        int leftMax = maxSubArrayHelper(nums,left,mid);
+        int rightMax = maxSubArrayHelper(nums,mid+1,right);
+
+        int crossMax = maxCrossingSubArray(nums,left,mid,right);
+
+        return Math.max(Math.max(rightMax, leftMax), crossMax);
+    }
+
+    private int maxCrossingSubArray(int[] nums, int left, int mid, int right){
+        int leftSum = Integer.MIN_VALUE;
+        int sum=0;
+
+        for(int i=mid; i>= left; i--){
+            sum += nums[i];
+            leftSum=Math.max(sum, leftSum);
+        }
+
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+
+        for(int j = mid+1; j<= right; j++){
+            sum += nums[j];
+            rightSum = Math.max(rightSum, sum);
+        }
+
+        return leftSum+rightSum;
+    }
 }
+    
+    
