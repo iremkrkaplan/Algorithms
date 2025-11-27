d361410a65a1eda2283f2d66f79fe40840d2c4cc
iremkrkaplan <iremkrkaplan@gmail.com>
2025-11-27 03:22:55 +0300

    Some command lines

--- DynamicProgramming/LeetCode62-UniquePaths/LeetCode62-UniquePaths.java
+++ DynamicProgramming/LeetCode62-UniquePaths/LeetCode62-UniquePaths.java
@@ -5,18 +5,23 @@ class Solution {
         dp[0][0] = 1; //base case
 
         for (int i = 0 ; i < m; i++){ //'i' represents the ROW (vertical position)
+
             for(int j=0; j<n; j++){ // 'j' represents the COLUMN (horizontal position)
+
                 if(i==0 && j ==0){
                     continue;
                 }
 
                 int val = 0;
+
                 if(i>0){ // Check if there is a row above (avoid IndexOutOfBounds)
                     val += dp[i-1][j]; // Grab value from the cell directly ABOVE
                 }
+
                 if(j>0){ // Check if there is a column to the left
                     val += dp[i][j-1]; // Grab value from the cell directly to the LEFT
                 }
+                
                 dp[i][j]=val;
             }
         }
